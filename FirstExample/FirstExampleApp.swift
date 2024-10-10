061065cf1287987500b0fe9c8b941d527a1e8fc3
Temel Gunaydin <tgunaydin@gmail.com>
2024-10-10 15:45:35 +0300

    Model Container is created

--- FirstExample/FirstExampleApp.swift
+++ FirstExample/FirstExampleApp.swift
@@ -6,12 +6,14 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct FirstExampleApp: App {
     var body: some Scene {
         WindowGroup {
             FirstExampleView()
+                .modelContainer(for: PersonModel.self)
         }
     }
 }
