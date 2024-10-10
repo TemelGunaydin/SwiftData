799115078c2dfe9796d7df4bd752d098b4e9c454
Temel Gunaydin <tgunaydin@gmail.com>
2024-10-10 15:56:05 +0300

    @Query is used in the view
    Now we can observe any changes in the modelContext. Because our model is
    observable by default

--- FirstExample/Views/FirstExampleView.swift
+++ FirstExample/Views/FirstExampleView.swift
@@ -12,13 +12,21 @@ struct FirstExampleView: View {
     
     @Environment(\.modelContext) private var modelContext
     
+    @Query private var people: [PersonModel]
+    
     var body: some View {
         Button("Add Person") {
             let name = ["Mark","Lem","Chase"].randomElement()!
             let person = PersonModel(name: name)
             modelContext.insert(person)
         }
+        List(people) { person in
+            Text(person.name)
+        }
+        
         .font(.title)
+        
+        
     }
 }
 
